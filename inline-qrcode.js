14ec0d2a64123ad1566bcbf36a08b1664ace1f3d
effgarces <effgarces@gmail.com>
2022-11-18 12:47:18 +0000

    Fixes the qrcode insertion

--- inline-qrcode.js
+++ inline-qrcode.js
@@ -1,13 +1,13 @@
 function qrcode_code() {
-  var url = $( '#copylink' ).attr('value');
+  var url = document.querySelector("#copylink").value;
   var qrcimg = 'http://chart.apis.google.com/chart?chs=150x150&cht=qr&chld=M&chl='+url;
-  var qrcimglnk = 'http://chart.apis.google.com/chart?chs=500x500&cht=qr&chld=M&chl='+url;	
+  var qrcimglnk = 'http://chart.apis.google.com/chart?chs=500x500&cht=qr&chld=M&chl='+url;
   var insertimg = "<div id='qrcode2' class='share'><a href="+qrcimglnk+"><img id='myid' src='" + qrcimg + "' /></a></div>"
-
+  console.log(url);
   if ( $( '#qrcode2' ).length > 0 ) {
     $( '#qrcode2' ).remove( );
   }
-  if ($( "#shareboxes" )) {  
+  if ($( "#shareboxes" )) {
     $( "#shareboxes" ).append( insertimg );        // Append new elements
     $( "div#qrcode2 img" ).css( "width", "100px" );
     $( "div#qrcode2 img" ).css( "height", "100px" );
@@ -15,4 +15,14 @@ function qrcode_code() {
 
 }
 
-$(document).ready( function(){ qrcode_code() } );
+$(document).ready( function( ){
+    // Share button behavior
+    $( '.button_share' ).on("click", function( ){
+        qrcode_code( );
+    });
+
+    // Tab behavior on stats page
+    $("a[href='#stat_tab_share']").on("click", function( ){
+        qrcode_code( );
+    });
+});
