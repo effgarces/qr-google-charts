690584e916ff9cd8c4d5bd8e34ece6d97ba8ff61
effgarces <effgarces@gmail.com>
2022-11-22 08:51:45 +0000

    Replaced Google Charts API
    Replaced the Google Charts API with Quickcharts, due to the deprecation of Google Charts API

--- inline-qrcode.js
+++ inline-qrcode.js
@@ -1,7 +1,7 @@
 function qrcode_code() {
-  var url = document.querySelector("#copylink").value;
-  var qrcimg = 'http://chart.apis.google.com/chart?chs=150x150&cht=qr&chld=M&chl='+url;
-  var qrcimglnk = 'http://chart.apis.google.com/chart?chs=500x500&cht=qr&chld=M&chl='+url;
+  var url = encodeURI(document.querySelector("#copylink").value);
+  var qrcimg = 'https://quickchart.io/qr?size=150x150&text='+url;
+  var qrcimglnk = 'https://quickchart.io/qr?size=500x500&text='+url;	
   var insertimg = "<div id='qrcode2' class='share'><a href="+qrcimglnk+"><img id='myid' src='" + qrcimg + "' /></a></div>"
   console.log(url);
   if ( $( '#qrcode2' ).length > 0 ) {
